09fa385c0d1b409aaa55f59aa4d5668d1c9caa58
Irakli Skhirtladze <iskhirtladzei@gmail.com>
2024-04-18 11:59:09 +0400

    changes in auth, fixed home redirect issues

--- tbc_colab/urls.py
+++ tbc_colab/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),  # New
+    path('accounts/', include('accounts.urls')),  # New
 ]
