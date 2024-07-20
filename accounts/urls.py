7cfd11a275f7b11f0422e48981b7346fa0f2926e
Irakli Skhirtladze <iskhirtladzei@gmail.com>
2024-07-20 18:50:20 +0400

    reformated dir structure

--- accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from accounts.views import ActivateUser
-
-urlpatterns = [
-    path('activate/<uid>/<token>/', ActivateUser.as_view(), name='activate-user'),
-]
