e4d7dbb20a81e7ef37f0e0291e4956c30f287d1f
Irakli Skhirtladze <iskhirtladzei@gmail.com>
2024-08-05 12:07:44 +0400

    railway conf added. some more changes

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,6 +1,2 @@
-# import os
 
-# # Choose the settings file based on the DJANGO_SETTINGS_MODULE environment variable
-# settings_module = os.getenv('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
-# exec(f"from {settings_module} import *")
 
