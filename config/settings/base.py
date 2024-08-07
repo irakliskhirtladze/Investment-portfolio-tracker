844112be1ea7c1fa58ad3ebfdfe144b6212ad0f1
Irakli Skhirtladze <iskhirtladzei@gmail.com>
2024-08-07 19:10:28 +0400

    test

--- config/settings/base.py
+++ config/settings/base.py
@@ -34,7 +34,8 @@ CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS',
                               cast=Csv())
 
 # Celery settings
-REDIS_URL = config('REDIS_URL', default='redis://redis:6379/0')
+CELERY_BROKER_URL = config('REDIS_URL', default='redis://redis:6379/0')
+CELERY_RESULT_BACKEND = config('REDIS_URL', default='redis://redis:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
