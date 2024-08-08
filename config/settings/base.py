4bf7f311a31f8340fa1b4d101192471cf0344acb
Irakli Skhirtladze <iskhirtladzei@gmail.com>
2024-08-08 13:14:26 +0400

    test

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Asia/Tblisi'
+TIME_ZONE = 'Asia/Tbilisi'
 TZ = pytz.timezone(TIME_ZONE)  # New
 USE_I18N = True
 USE_TZ = True
